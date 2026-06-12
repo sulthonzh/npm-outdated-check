765b32447c42628c8a4daff1757a30a30fb10663
Sulthonzh <sulthonzh@users.noreply.github.com>
2026-06-12 18:18:11 +0700

    Add ESLint configuration for TypeScript linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  env: {
+    node: true,
+    es2022: true
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'no-console': 'warn',
+    'eqeqeq': 'error',
+    'curly': 'error',
+    'brace-style': 'error'
+  }
+};
